05fe73b833eb89d30ad10317495f905ab8bc5a5d
seNNNz0rel <67145369+seNNNz0rel@users.noreply.github.com>
2023-10-11 19:55:45 +0300

    made routing for home page

---
+++ frontend/my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
